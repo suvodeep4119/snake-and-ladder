d7b20d030437b65a624e2ac1824c73f3c0cc80cf
suvodeep4119 <suvodeep4119@gmail.com>
2020-02-04 12:54:02 +0530

    ADDED:Player move feature

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -3,6 +3,9 @@ echo "Welcome to THE SNAKE AND LADDER GAME"
 
 #CONSTANTS
 #NO_OF_PLAYERS=1 #Just a comment not a constant
+NO_PLAY=0
+SNAKE=1
+LADDER=2
 
 
 #VARIABLES
@@ -14,4 +17,21 @@ function rollDie()
 {
    dieValue=$(( (RANDOM%6)+1 ))
 }
-rollDie
+
+function playerMove()
+{
+	rollDie
+	case $((RANDOM%3)) in
+	   $NO_PLAY)
+	      playerPosition=$playerPosition
+	      ;;
+	   $SNAKE)
+	      playerPosition=$(($playerPosition-$dieValue))
+	      ;;
+	   $LADDER)
+	      playerPosition=$(($playerPosition+$dieValue))
+	      ;;
+	esac
+}
+
+playerMove
